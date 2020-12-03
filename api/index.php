34416bd30b93a75af8969c6f8a9dc909c41247c9
alexbcberio <alexbcberio@gmail.com>
2020-12-03 15:01:23 +0100

    ✨ Added new page WIP
    Co-authored-by: AsierCruz <asiercruz99@gmail.com>
    Co-authored-by: Rubikivan <ivangarona@gmail.com>

---
+++ api/index.php
@@ -0,0 +1,24 @@
+<?php
+
+function endsWith( $haystack, $needle ) {
+  $length = strlen( $needle );
+  if( !$length ) {
+      return true;
+  }
+  return substr( $haystack, -$length ) === $needle;
+}
+
+$uri = $_SERVER["REQUEST_URI"];
+
+if (endsWith($uri, "/")) {
+  $uri .= "index.php";
+}
+
+$uri = realpath(__DIR__ . "/.." . $uri);
+
+if (!file_exists($uri)) {
+  header("Status: 404 Not Found");
+
+} else {
+  require($uri);
+}
